5513cb17c3d92fea45d17a62f1dbaf4fb8a9982e
Lucas Tavares <lucastavares@localhost.localdomain>
2022-05-18 17:58:08 -0300

    Exemplo funcional de soma de constante em vetor

--- exemplo-de-soma-vet/main.py
+++ exemplo-de-soma-vet/main.py
@@ -1,18 +1,22 @@
 import sys
 
+from regex import P
+
 sys.path.append("..")
 from pyDF.pydf import *
+from pyDF.nodes import Feeder
 
 
 nprocs = int(sys.argv[1]) # 3
 
-vetor = [1, 2, 5, 6, 7, 9, 2, 6, 2, 6, 1, 12]
+vector = [1, 2, 5, 6, 7, 9, 2, 6, 2, 6, 1, 12]
 # subs
   # [1, 2, 5, 6]
   # [7, 9, 2, 6]
   # [2, 6, 1, 12]
 
-def soma_vet(const, vet):
+def add_to_vet(args):
+  const, vet = args[0][0], args[0][1] 
   new_sub_vet = []
   for item in vet:
     new_sub_vet += [const + item]
@@ -22,7 +26,7 @@ def soma_vet(const, vet):
 def join_soma(args):
   new_vet = []
   for partial in args:
-    new_vet += [partial]
+    new_vet += partial
   print(new_vet)
 
 def split(vector, num_parts):
@@ -32,6 +36,22 @@ def split(vector, num_parts):
     new_vet += [vector[i * quotient + min(i, rest):(i + 1) * quotient + min(i + 1, rest)]]
   return new_vet
 
+vector_splited = split(vector, nprocs)
 
-print(soma_vet(5, vetor))
-print(split(vetor, 5))
+graph = DFGraph()
+
+result_node = Node(join_soma, nprocs)
+graph.add(result_node)
+
+for i in range(nprocs):
+  add_to_vet_feeder = Feeder([10, vector_splited[i]])
+  parcial_add = Node(add_to_vet, 1)
+
+  graph.add(add_to_vet_feeder) 
+  graph.add(parcial_add)
+  
+  add_to_vet_feeder.add_edge(parcial_add, 0)
+  parcial_add.add_edge(result_node, i)
+
+scheduler = Scheduler(graph, nprocs, mpi_enabled=False)
+scheduler.start()
